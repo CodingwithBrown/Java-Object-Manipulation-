d4f44d84f69e5ba4c281a0338cbf8ec8ad477458
Jayden <Jbro86@outlook.com>
2025-05-18 00:37:34 -0700

    Update sculptureListLengths.js
    Extended the code to add the lengths of the objects

--- sculptureListLengths.js
+++ sculptureListLengths.js
@@ -1,10 +1,30 @@
-// For loop, to loop the data.js values
-for (let i = 0; i < sculptureListLengths.length; i++) {
-    const sculptureListLengths = artworks[i];
-    listLengths[sculptureListLengths.name] = {
-      nameLength: sculptureListLengths.name.length,
-      descriptionLength: sculptureListLengths.description.length
-    };
+// copied from git
+const sculptureList = [
+  {
+  name: 'Homenaje a la Neurocirugía',
+  artist: 'Marta Colvin Andrade',
+  description: 'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+  url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+  alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'  
+}
+];
+
+// Empty container for return values
+const sculptureListLengths = {};
+
+// Section to process each sculpture object in the container
+for (const sculpture of sculptureList) {
+  // Create the length of each object
+  const lengthsObj = {};
+  
+  // Set the length to the object to equal the sculpture
+  for (const key in sculpture) {
+    lengthsObj[key] = sculpture[key].length;
   }
   
-  console.log(sculptureListLengths); // "Go Live Server" keeps opening my directory not my root file FIX!!!
+  // Will add the objects length to the results.
+  Object.assign(sculptureListLengths, lengthsObj);
+}
+
+// Displays the results
+console.log(sculptureListLengths);
